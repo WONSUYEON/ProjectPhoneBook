51a88285a5c667e6d391c44b46d9d1b1d7d72422
tndus910224 <tndus910224@gmail.com>
2021-10-26 17:53:24 +0900

    10/26 첫날

---
+++ src/project1/PhoneBookVer02.java
@@ -0,0 +1,17 @@
+package project1;
+
+import java.util.Scanner;
+
+import project1.ver02.PhoneInfo;
+
+public class PhoneBookVer02
+{
+
+	public static void main(String[] args)
+	{	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("선택하세요...");
+		
+	}
+
+}
